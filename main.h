41a8a72a664d96b8a191bba8b53f4b7158df435f
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-08-14 12:06:22 +0200

    libraries added

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>    /* for access, chdir, close, execve, _exit, fork, getpid
+			  , isatty, read, write, and other POSIX functions */
+#include <dirent.h>    /* for closedir, opendir, readdir */
+#include <stdio.h>     /* for fflush, perror, printf, fprintf, vfprintf,
+			  sprintf, putchar, getline */
+#include <stdlib.h>    /* for exit, free, malloc */
+#include <sys/types.h> /* for pid_t and other system types */
+#include <sys/stat.h>  /* for stat, lstat, fstat */
+#include <fcntl.h>     /* for open */
+#include <signal.h>    /* for kill, signal */
+#include <sys/wait.h>  /* for wait, waitpid, wait3, wait4 */
+#include <string.h>    /* for strtok and more full library allowed */
+
+
+#endif
