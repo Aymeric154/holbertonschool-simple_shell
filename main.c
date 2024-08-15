23a4b9ac6442f31020c782924f44296a9ac0ccf0
Aymeric154 <aymeric.gelle@outlook.fr>
2024-08-15 14:09:14 +0200

    entry point of the program simple shell

---
+++ main.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * main -
+ *
+ */
+int main(void)
+{
+	char line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+
+	signal(SIGINT, SIG_IGN); /* Ignore Ctrl+C */
+
+		while (1)
+		{
+			prompt();
+			nread = getline(&line, &len, stdin);
+
+			if (nread == -1) /* Handle EOF (Ctrl+D) */
+			{
+				free(line);
+				exit(EXIT_SUCCESS);
+			}
+
+			if (line[nread - 1] == '\n') /* Remove trailing newline character */
+				line[nread - 1] = '\0';
+
+			if (is_builtin_command(line))
+			{
+				continue; /* Execute built-in commands directly */
+			}
+
+			execute_command(line);
+		}
+
+	free(line);
+	return (0);
+}
