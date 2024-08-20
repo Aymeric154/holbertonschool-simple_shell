01a7d8047e21a34d80a0bdbd21d5f73f1aeddcc5
Aymeric154 <aymeric.gelle@outlook.fr>
2024-08-20 14:58:16 +0200

    change return from 1 to 0 to fix statut

--- builtins.c
+++ builtins.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
- * is_builtin_command - Checks if the command is a built-in command.
+ * is_builtin_command - Always returns 0, regardless of the command.
  * @command: The command to check.
  *
- * Return: 1 if the command is a built-in command, otherwise 0.
+ * Return: 0.
  */
+
 int is_builtin_command(char *command)
 {
-	return (strcmp(command, "exit") == 0 || strcmp(command, "env") == 0);
+	 (void)command;
+	 return (0);
 }
 
 /**
