a8a57441bffaebd4836a9c5a1fa617da3fbca17f
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-08-21 13:15:09 +0200

    execute_command.c fixed , main.h updated, parse.c created to handle options

--- execute_command.c
+++ execute_command.c
@@ -1,74 +1,54 @@
 #include "main.h"
-/**
- * execute_command - Executes a command in a child process.
- * @command: The command to be executed.
- */
+
 void execute_command(char *command)
 {
-	pid_t pid;
-	int status;
-	char *argv[2];
-	char *cmd_path;
+    pid_t pid;
+    int status;
+    char **argv;
+    char *cmd_path;
 
-	if (command == NULL || *command == '\0')
-		return;
+    argv = parse_command(command);  /* Parse the command string */
 
-	if (strcmp(command, "exit") == 0)
-	{
-		exit(EXIT_SUCCESS);
-	}
+    if (argv == NULL || argv[0] == NULL)
+    {
+        free(argv);
+        return;
+    }
 
-	if (command[0] == '/' || command[0] == '.')
-	{
-		cmd_path = command;
-	}
-	else
-	{
-		cmd_path = find_command_in_path(command);
-		if (cmd_path == NULL)
-		{
-			fprintf(stderr, "./shell: %s: command not found\n", command);
-			return;
-		}
-	}
+    if (argv[0][0] == '/' || argv[0][0] == '.')
+        cmd_path = argv[0];  /* Absolute or relative path */
+    else
+    {
+        cmd_path = find_command_in_path(argv[0]);
+        if (cmd_path == NULL)
+        {
+            write(2, "Command not found\n", 18);  /* Handle error */
+            free(argv);
+            return;
+        }
+    }
 
-	argv[0] = cmd_path;
-	argv[1] = NULL;
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("fork");
+        free(argv);
+        exit(EXIT_FAILURE);
+    }
+    else if (pid == 0)  /* Child process */
+    {
+        if (execve(cmd_path, argv, environ) == -1)
+        {
+            perror("./shell");
+            free(argv);
+            _exit(EXIT_FAILURE);
+        }
+    }
+    else  /* Parent process */
+        wait(&status);
 
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("fork");
-		if (cmd_path != command)
-			free(cmd_path);
-		return;
-	}
-	else if (pid == 0)
-	{
-		if (execve(cmd_path, argv, environ) == -1)
-		{
-			perror("./shell");
-			if (cmd_path != command)
-				free(cmd_path);
-			_exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		if (wait(&status) == -1)
-		{
-			perror("wait");
-			return;
-		}
-
-		if (WIFEXITED(status))
-		{
-			int exit_status = WEXITSTATUS(status);
-			if (exit_status != 0)
-				fprintf(stderr, "./shell: Child exited with status %d\n", exit_status);
-		}
-	}
-
-	if (cmd_path != command)
-		free(cmd_path);
+    if (cmd_path != argv[0])
+        free(cmd_path);
+    free(argv);  /* Free the argument array */
 }
+
